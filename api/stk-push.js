a5c07cebf8aa44d232fb64a21303d149ec76e26c
KelluuhShit <kronoh60@gmail.com>
2025-12-02 10:26:13 +0300

    new sco cred

--- api/stk-push.js
+++ api/stk-push.js
@@ -57,7 +57,7 @@ module.exports = async (req, res) => {
     const payload = {
       amount: Number(amount),
       phone_number: formattedPhone,
-      channel_id: 4377,
+      channel_id: 4456,
       provider: 'm-pesa',
       external_reference: reference,
       callback_url: callbackUrl,
